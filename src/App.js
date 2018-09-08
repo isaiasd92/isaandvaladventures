58e6185caa17ff96bccfa2f529e992d26993f6f2
Isaias <isaiasdelgado03@gmail.com>
2018-09-08 03:09:32 -0500

    Fixed the issue with the map not working correctly.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
+
 import React, { Component } from 'react';
 import { bubble as Menu } from 'react-burger-menu';
 import { Parallax, Background } from 'react-parallax';
-import ReactMapGL, {NavigationControl} from 'react-map-gl';
+import MapGL, {NavigationControl} from 'react-map-gl';
 
-/* CSS */
+// CSS 
 import 'react-sticky-header/styles.css';
 import './App.css';
 
+// Environment Variables 
 import {MAP_TOKEN, MAP_STYLE} from './config';
-
 require ('dotenv').config();
-
+{/*
 class App extends Component {
   render() {
     const {updateViewport} = this.props;
@@ -21,48 +22,146 @@ class App extends Component {
           <a id="about" className="menu-item" href="/about">About</a>
           <a id="contact" className="menu-item" href="/contact">Contact</a>
         </Menu>
-        <link href="https://fonts.googleapis.com/css?family=Courgette" rel="stylesheet" />
-        <link href='https://api.tiles.mapbox.com/mapbox-gl-js/v0.42.0/mapbox-gl.css' rel='stylesheet' />
-        <body>
-          <div>
-            <Parallax
-              bgImage={require('./img/space.jpg')}
-              bgImageAlt="the cat"
-              strength={200}
-            >
-            <h1 className="App-header">IsaAndValAdventures</h1>
-              <div style={{ height: '100px'}} />
-            </Parallax>
-            <Parallax strength={300}>
-              <div>
-                Use the background component for custom elements
-              </div>
-              <Background className="custom-bg">
-              </Background>
-            </Parallax>
-          </div>
-          <div>
-            <ReactMapGL 
-              mapStyle={MAP_STYLE}
-              width={400}
-              height={400}
-              latitude={31.169621}
-              longitude={-99.683617}
-              zoom={3}
-              onViewportChange = {(viewport) => {
-                const {width, height, latitude, longitude, zoom} = viewport;
-              }}
-              mapboxApiAccessToken={MAP_TOKEN}
-            >
-              <div style={{position: 'absolute', right: 0}}>
-                <NavigationControl onViewportChange={updateViewport} />
-              </div>
-            </ReactMapGL>
-          </div>
-        </body>
+        <div id="banner">
+          <Parallax
+            bgImage={require('./img/space.jpg')}
+            bgImageAlt="the cat"
+            strength={200}
+          >
+          <h1 className="App-header">IsaAndValAdventures</h1>
+            <div style={{ height: '100px'}} />
+          </Parallax>
+          <Parallax strength={300}>
+            <div>
+              Use the background component for custom elements
+            </div>
+            <Background className="custom-bg">
+            </Background>
+          </Parallax>
+        </div>
+        <div id="map">
+          <MapGL 
+            mapStyle={MAP_STYLE}
+            width={400}
+            height={400}
+            latitude={31.169621}
+            longitude={-99.683617}
+            zoom={3}
+            onViewportChange = {(viewport) => {
+              const {width, height, latitude, longitude, zoom} = viewport;
+            }}
+            mapboxApiAccessToken={MAP_TOKEN}
+          >
+            <div style={{position: 'absolute', right: 0}}>
+              <NavigationControl onViewportChange={updateViewport} />
+            </div>
+          </MapGL>
+        </div>
       </div>
     );
   }
 }
 
 export default App;
+*/}
+
+export default class App extends Component {
+
+  state = {
+    viewport: {
+      latitude: 37.729,
+      longitude: -122.36,
+      zoom: 11,
+      bearing: 0,
+      pitch: 50,
+      width: 500,
+      height: 500
+    },
+    interactionState: {},
+    settings: {
+      dragPan: true,
+      dragRotate: true,
+      scrollZoom: true,
+      touchZoom: true,
+      touchRotate: true,
+      keyboard: true,
+      doubleClickZoom: true,
+      minZoom: 0,
+      maxZoom: 20,
+      minPitch: 0,
+      maxPitch: 85
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this._resize);
+    this._resize();
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this._resize);
+  }
+
+  _resize = () => {
+    this.setState({
+      viewport: {
+        ...this.state.viewport,
+        width: this.props.width || window.innerWidth,
+        height: this.props.height || window.innerHeight
+      }
+    });
+  };
+
+  _onViewportChange = viewport => this.setState({viewport});
+
+  _onInteractionStateChange = interactionState => this.setState({interactionState});
+
+  _onSettingChange = (name, value) => this.setState({
+    settings: {...this.state.settings, [name]: value}
+  });
+
+  render() {
+
+    const {viewport, settings, interactionState} = this.state;
+
+    return (
+      <div className="App">
+      <div className="Menu">
+        <Menu>
+          <a id="home" className="menu-item" href="/">Home</a>
+          <a id="about" className="menu-item" href="/about">About</a>
+          <a id="contact" className="menu-item" href="/contact">Contact</a>
+        </Menu>
+      </div>
+      <div className="banner">
+        <Parallax
+          bgImage={require('./img/space.jpg')}
+          bgImageAlt="Space Image"
+          strength={200}
+        >
+        <h1 className="App-header">IsaAndValAdventures</h1>
+          <div style={{ height: '100px'}} />
+        </Parallax>
+        <Parallax strength={300}>
+          <div>
+            Use the background component for custom elements
+          </div>
+          <Background className="custom-bg">
+          </Background>
+        </Parallax>
+      </div>
+      <div className="Map">
+        <MapGL
+          {...viewport}
+          {...settings}
+          mapStyle={MAP_STYLE}
+          onViewportChange={this._onViewportChange}
+          onInteractionStateChange={this._onInteractionStateChange}
+          mapboxApiAccessToken={MAP_TOKEN} >
+        </MapGL>
+      </div>
+      </div>
+    );
+  }
+
+}
