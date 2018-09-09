b0e4639158092ad2572c780bc4aa8a6937d0777f
Isaias <isaiasdelgado03@gmail.com>
2018-09-09 10:33:44 -0500

    Fixed the navigationcontrol from being hidden behind the scroll bar on desktop browser

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import React, { Component } from 'react';
 import { bubble as Menu } from 'react-burger-menu';
 import { Parallax, Background } from 'react-parallax';
@@ -11,71 +10,17 @@ import './App.css';
 // Environment Variables 
 import {MAP_TOKEN, MAP_STYLE} from './config';
 require ('dotenv').config();
-{/*
-class App extends Component {
-  render() {
-    const {updateViewport} = this.props;
-    return (
-      <div className="App">
-        <Menu>
-          <a id="home" className="menu-item" href="/">Home</a>
-          <a id="about" className="menu-item" href="/about">About</a>
-          <a id="contact" className="menu-item" href="/contact">Contact</a>
-        </Menu>
-        <div id="banner">
-          <Parallax
-            bgImage={require('./img/space.jpg')}
-            bgImageAlt="the cat"
-            strength={200}
-          >
-          <h1 className="App-header">IsaAndValAdventures</h1>
-            <div style={{ height: '100px'}} />
-          </Parallax>
-          <Parallax strength={300}>
-            <div>
-              Use the background component for custom elements
-            </div>
-            <Background className="custom-bg">
-            </Background>
-          </Parallax>
-        </div>
-        <div id="map">
-          <MapGL 
-            mapStyle={MAP_STYLE}
-            width={400}
-            height={400}
-            latitude={31.169621}
-            longitude={-99.683617}
-            zoom={3}
-            onViewportChange = {(viewport) => {
-              const {width, height, latitude, longitude, zoom} = viewport;
-            }}
-            mapboxApiAccessToken={MAP_TOKEN}
-          >
-            <div style={{position: 'absolute', right: 0}}>
-              <NavigationControl onViewportChange={updateViewport} />
-            </div>
-          </MapGL>
-        </div>
-      </div>
-    );
-  }
-}
-
-export default App;
-*/}
 
 export default class App extends Component {
-
   state = {
     viewport: {
-      latitude: 37.729,
-      longitude: -122.36,
-      zoom: 11,
+      latitude: 31.169621,
+      longitude: -99.683617,
+      zoom: 5,
       bearing: 0,
       pitch: 50,
-      width: 500,
-      height: 500
+      width: 400,
+      height: 400
     },
     interactionState: {},
     settings: {
@@ -122,46 +67,48 @@ export default class App extends Component {
 
   render() {
 
-    const {viewport, settings, interactionState} = this.state;
+    const {viewport, settings} = this.state;
 
     return (
       <div className="App">
-      <div className="Menu">
-        <Menu>
-          <a id="home" className="menu-item" href="/">Home</a>
-          <a id="about" className="menu-item" href="/about">About</a>
-          <a id="contact" className="menu-item" href="/contact">Contact</a>
-        </Menu>
-      </div>
-      <div className="banner">
-        <Parallax
-          bgImage={require('./img/space.jpg')}
-          bgImageAlt="Space Image"
-          strength={200}
-        >
-        <h1 className="App-header">IsaAndValAdventures</h1>
-          <div style={{ height: '100px'}} />
-        </Parallax>
-        <Parallax strength={300}>
-          <div>
-            Use the background component for custom elements
-          </div>
-          <Background className="custom-bg">
-          </Background>
-        </Parallax>
-      </div>
-      <div className="Map">
-        <MapGL
-          {...viewport}
-          {...settings}
-          mapStyle={MAP_STYLE}
-          onViewportChange={this._onViewportChange}
-          onInteractionStateChange={this._onInteractionStateChange}
-          mapboxApiAccessToken={MAP_TOKEN} >
-        </MapGL>
-      </div>
+        <div className="Menu">
+          <Menu>
+            <a id="home" className="menu-item" href="/">Home</a>
+            <a id="about" className="menu-item" href="/about">About</a>
+            <a id="contact" className="menu-item" href="/contact">Contact</a>
+          </Menu>
+        </div>
+        <div className="banner">
+          <Parallax
+            bgImage={require('./img/space.jpg')}
+            bgImageAlt="Space Image"
+            strength={200}
+          >
+          <h1 className="App-header">IsaAndValAdventures</h1>
+            <div style={{ height: '100px'}} />
+          </Parallax>
+          <Parallax strength={300}>
+            <div>
+              Use the background component for custom elements
+            </div>
+            <Background className="custom-bg">
+            </Background>
+          </Parallax>
+        </div>
+        <div className="Map">
+          <MapGL
+            {...viewport}
+            {...settings}
+            mapStyle={MAP_STYLE}
+            onViewportChange={this._onViewportChange}
+            onInteractionStateChange={this._onInteractionStateChange}
+            mapboxApiAccessToken={MAP_TOKEN} >
+              <div style={{position: 'absolute', right: 20, top: 5}}>
+                <NavigationControl onViewportChange={this._onViewportChange} />
+              </div>
+          </MapGL>
+        </div>
       </div>
     );
   }
-
 }
